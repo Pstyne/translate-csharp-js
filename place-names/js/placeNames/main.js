c8a58697b5fba6a0848efd3fcb490e6f35e9a6e8
Andrew Birt <abirt1986@gmail.com>
2022-01-18 20:36:51 -0500

    translate place names code from c# to js

--- place-names/js/placeNames/main.js
+++ place-names/js/placeNames/main.js
@@ -1,1 +1,27 @@
-// Put your code here
+// JS is a dynamic language where specifying the data type
+// is not necessary
+
+const names = ["Nashville", "Hong Kong", "The back yard", 
+"Earth", "London", "The mall", "Ryman Auditorium", 
+"The Great Wall of China"];
+
+// console.log to output to console in js
+console.log("All Place Names");
+
+// JS uses for as its primitive iteration function
+for (const name of names) {
+  console.log(name);
+}
+
+// What happens when you console.log without an argument?
+// Didn't notice any output so using regex instead
+console.log('\n');
+
+// Filter the list to create a new list that only
+// has place names that start with the word "The".
+const theNames = names.filter(n => n.startsWith("The"));
+
+console.log("'The' Place Names");
+for (const name of theNames) {
+  console.log(name);
+}
